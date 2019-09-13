1f3c66945f7789598be75abe966ea12b7668717f
SpivEgin <spivegin@txtsme.com>
2019-09-13 15:48:40 -0400

    updated caddy build for go mod and go ver  1.13

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,16 @@
-FROM quay.io/spivegin/golang:v1.11.4 AS build-env-go110
-WORKDIR /opt/src/src/github.com/mholt
-ADD files/caddy_mods/caddyhttp.go.txt /tmp/caddyhttp.go
-ADD files/caddy_mods/run.go.txt /tmp/run.go
-ADD files/caddy_mods/go.mod.txt /tmp/go.mod
-
+FROM quay.io/spivegin/golang:v1.13 AS build-env-go113
+WORKDIR /opt/caddy_build/
+ADD files/caddy_mods/main.go /opt/caddy_build/
 RUN apt-get update && apt-get install -y gcc &&\
-    go get github.com/caddyserver/builds &&\
-    go get github.com/mholt/caddy
-
-ENV GO111MODULE=on
-RUN cd caddy &&\
-    cp /tmp/run.go ${GOPATH}/src/github.com/mholt/caddy/caddy/caddymain/ &&\
-    cp /tmp/caddyhttp.go ${GOPATH}/src/github.com/mholt/caddy/caddyhttp/ &&\
-    cp /tmp/go.mod ${GOPATH}/src/github.com/mholt/caddy/go.mod &&\
-    go mod tidy && go mod vendor
-ENV GO111MODULE=off
-RUN git clone https://github.com/mholt/certmagic.git
-RUN cd caddy/caddy && go run build.go
+    go mod init caddy &&\
+    go get github.com/caddyserver/caddy &&\
+    go build
 
 FROM quay.io/spivegin/tlmbasedebian
 ENV DINIT=1.2.2 
 WORKDIR /opt/tlm
 RUN mkdir /opt/bin 
-COPY --from=build-env-go110 /opt/src/src/github.com/mholt/caddy/caddy/caddy /opt/bin/caddy
+COPY --from=build-env-go113 /opt/caddy_build/caddy /opt/bin/caddy
 ADD https://github.com/Yelp/dumb-init/releases/download/v${DINIT}/dumb-init_${DINIT}_amd64.deb /tmp/dumb-init_amd64.deb
 
 ADD https://raw.githubusercontent.com/adbegon/pub/master/AdfreeZoneSSL.crt /usr/local/share/ca-certificates/
---
+++ files/caddy_mods/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+        "github.com/caddyserver/caddy/caddy/caddymain"
+
+        // plug in plugins here, for example:
+        // _ "import/path/here"
+        // plug in the standard directives
+        _ "github.com/caddyserver/caddy/caddyhttp/basicauth"
+        _ "github.com/caddyserver/caddy/caddyhttp/bind"
+
+        //      _ "github.com/caddyserver/caddy/caddyhttp/browse"
+        _ "github.com/caddyserver/caddy/caddyhttp/errors"
+        _ "github.com/caddyserver/caddy/caddyhttp/expvar"
+        _ "github.com/caddyserver/caddy/caddyhttp/extensions"
+        _ "github.com/caddyserver/caddy/caddyhttp/fastcgi"
+        _ "github.com/caddyserver/caddy/caddyhttp/gzip"
+        _ "github.com/caddyserver/caddy/caddyhttp/header"
+
+        //      _ "github.com/caddyserver/caddy/caddyhttp/index"
+        _ "github.com/caddyserver/caddy/caddyhttp/internalsrv"
+        _ "github.com/caddyserver/caddy/caddyhttp/limits"
+        _ "github.com/caddyserver/caddy/caddyhttp/log"
+
+        //      _ "github.com/caddyserver/caddy/caddyhttp/markdown"
+        _ "github.com/caddyserver/caddy/caddyhttp/mime"
+        _ "github.com/caddyserver/caddy/caddyhttp/pprof"
+        _ "github.com/caddyserver/caddy/caddyhttp/proxy"
+        _ "github.com/caddyserver/caddy/caddyhttp/push"
+        _ "github.com/caddyserver/caddy/caddyhttp/redirect"
+        _ "github.com/caddyserver/caddy/caddyhttp/requestid"
+        _ "github.com/caddyserver/caddy/caddyhttp/rewrite"
+        _ "github.com/caddyserver/caddy/caddyhttp/root"
+        _ "github.com/caddyserver/caddy/caddyhttp/status"
+        _ "github.com/caddyserver/caddy/caddyhttp/templates"
+        _ "github.com/caddyserver/caddy/caddyhttp/timeouts"
+        _ "github.com/caddyserver/caddy/caddyhttp/websocket"
+        _ "github.com/caddyserver/caddy/onevent"
+        // 3rd party plugins
+        // _ "github.com/caddyserver/dnsproviders/digitalocean"
+        // docker proxy
+        //_ "github.com/lucaslorentz/caddy-docker-proxy/plugin"
+        // minify
+        //_ "github.com/hacdias/caddy-minify"
+        // CORS
+        //_ "github.com/captncraig/cors"
+)
+
+func main() {
+        // optional: disable telemetry
+        caddymain.EnableTelemetry = false
+        caddymain.Run()
+}
